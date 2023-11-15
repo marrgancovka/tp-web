6f2a38fe33c43e95f1d156c9b4308cb89dd31b00
Margarita <altsybeevam@yandex.ru>
2023-11-15 13:43:45 +0300

    auth & registration frontend

---
+++ inst_backend/app/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class ProfileManager(models.Manager):
+
+    def get_user_by_username(self, username):
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            user = None
+
+        return user
+
+
+class Profiles(models.Model):
+    objects = ProfileManager()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars', default='image/avatars/default.jpg', blank=True, null=True)
+    nickname = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+class Moments(models.Model):
+    content = models.CharField(max_length=1000, blank=True, null=True)
+    author = models.ForeignKey('Profiles', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='moments')
+    date_creation = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tags', blank=True, related_name='tags')
+    is_delete = models.BooleanField(default=False)
+
+class Comments(models.Model):
+    content = models.CharField(max_length=1000)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('Profiles', on_delete=models.CASCADE)
+    moment = models.ForeignKey('Moments', on_delete=models.CASCADE)
+    is_delete = models.BooleanField(default=False)
+
+class Subscriptions(models.Model):
+    author=models.ForeignKey('Profiles', on_delete=models.CASCADE, related_name='author_subscription')
+    subscriber = models.ForeignKey('Profiles', on_delete=models.CASCADE, related_name='subscriber_subscription')
+    date_subscription = models.DateTimeField(auto_now_add=True)
+    is_delete = models.BooleanField(default=False)
+
+class LikesMoment(models.Model):
+    author = models.ForeignKey('Profiles', on_delete=models.CASCADE)
+    moment = models.ForeignKey('Moments', on_delete=models.CASCADE)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    is_delete = models.BooleanField(default=False)
+
+class LikesComment(models.Model):
+    author = models.ForeignKey('Profiles', on_delete=models.CASCADE)
+    comment = models.ForeignKey('Comments', on_delete=models.CASCADE)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    is_delete = models.BooleanField(default=False)
+
+class Tags(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    def __str__(self):
+        return f'{self.title}'
+    
---
+++ inst_backend/instagram/urls.py
@@ -0,0 +1,52 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from rest_framework import routers
+from app import views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+router = routers.DefaultRouter()
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+   #  path('auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+
+    path(r'tags/', views.get_tags_list, name="tag list"),
+    path(r'tags/<int:pk>', views.get_tags_by_id, name="tag by id"),
+    path(r'tags/new/', views.new_tags, name="new tag"),
+
+    path(r'comments/<int:pk>', views.get_comment_by_id, name="comment by id"),
+    path(r'comments/delete/', views.delete_comment, name="delete comment"),
+    path(r'comments/new/', views.new_comment, name="new comment"),
+    path(r'comments/from/moment/<int:pk>', views.get_comments_from_moment, name="get comment from moment"),
+    path(r'comments/from/user', views.get_comments_from_user, name="get comment from user"),
+
+    path(r'moments/', views.get_moments_list, name="get moments list"),
+    path(r'moments/new/', views.new_moment, name="new moment"),
+    path(r'moments/<int:pk>', views.get_moment_by_id, name="get moment by id"),
+    path(r'moments/delete/<int:pk>', views.delete_moment, name="delete moment")
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
